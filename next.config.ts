933e77db160e9a94b08eb97b68648d7e806cc1f6
Min Kaung Kyaw <minkaungkyaw@yomafleet.com>
2026-01-31 09:29:33 +0630

    feat(css): add css varaiable and configuration in global.css

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+import mdx from "@next/mdx";
 import type { NextConfig } from "next";
+
+const withMDX = mdx();
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    removeConsole: isProd,
+    reactRemoveProperties: isProd,
+  },
+  experimental: {
+    cssChunking: true,
+    mdxRs: true,
+    taint: true,
+    useLightningcss: true,
+    browserDebugInfoInTerminal: !isProd,
+  },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
